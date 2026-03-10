3cd851706880c8bfa744c7cc41cf3007ae66ee29
Julia-Abramova <yulia.abramova14@yandex.ru>
2026-03-10 15:45:08 +0300

    Update Main.java
    все лишнее убрано

--- MyDemoy/src/main/java/org/example/Main.java
+++ MyDemoy/src/main/java/org/example/Main.java
@@ -4,26 +4,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Reading list of persons from YAML \n");
-
         YamlReader reader = new YamlReader();
 
-        try {
-            List<Person> persons = reader.readPersons();
+        List<Person> persons = reader.readPersons();
 
-            System.out.println("Found records: " + persons.size());
-
-            if (persons.isEmpty()) {
-                System.out.println("The list is empty");
-            } else {
-                for (Person p : persons) {
-                    System.out.println(p);
-                }
-            }
-
-        } catch (Exception e) {
-            System.err.println("Error while working with YAML:");
-            e.printStackTrace();
-        }
+        if (persons.isEmpty())
+            System.out.println("The list is empty");
+        else 
+            for (Person p : persons) 
+                System.out.println(p);
     }
 }
